8854ae65da1510c86e46b2f1fbc92e227efb518e
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-11-13 13:27:55 +0300

    [Enh]: Added chart and title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useMemo, useState } from 'react'
 import { Input } from './input/input';
 import { Tr } from './table/tr';
 import './App.css'
+import { Chart } from './chart/chart';
+import { TitlePage } from './title-page/title-page';
 
 // FIX: Limit.
 function factorial(n: number): number {
@@ -168,73 +170,73 @@ const DEFAULT = {
 
 interface InputError { label: string, message: string };
 
-function App() {
-  const [errors, setErrors] = useState<InputError[]>([])
-  const setError = (error: InputError) => setErrors((errors) => [...errors, error])
-  const clearErrorsForInput = (label: string) => setErrors((errors) => errors.filter((error) => error.label !== label))
+function MainPage() {
+    const [errors, setErrors] = useState<InputError[]>([])
+    const setError = (error: InputError) => setErrors((errors) => [...errors, error])
+    const clearErrorsForInput = (label: string) => setErrors((errors) => errors.filter((error) => error.label !== label))
 
-  const [tno, setTno] = useState(DEFAULT.tno)
-  const onTnoChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTno(Number(e.target.value))
-  }
-
-  const [to, setTo] = useState(DEFAULT.to)
-  const onToChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTo(Number(e.target.value))
-  }
-
-  const [N, setN] = useState(DEFAULT.N)
-  const onNChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const N = Number(e.target.value);
-
-    if (N > 101) {
-        return setError({ label: "N", message: "Слишком большое значение для N! Сделайте, пожалуйста, его меньше или равным 100" })
+    const [tno, setTno] = useState(DEFAULT.tno)
+    const onTnoChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTno(Number(e.target.value))
     }
 
-    if (N < 0) {
-        return setError({ label: "N", message: "Сделайте, пожалуйста, N неотрицательным" })
+    const [to, setTo] = useState(DEFAULT.to)
+    const onToChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTo(Number(e.target.value))
     }
 
-    clearErrorsForInput('N')
-    setN(N)
-  }
+    const [N, setN] = useState(DEFAULT.N)
+    const onNChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const N = Number(e.target.value);
 
-  const [C1, setC1] = useState(DEFAULT.C1)
-  const onC1Change = (e: ChangeEvent<HTMLInputElement>) => {
-    setC1(Number(e.target.value))
-  }
+        if (N > 101) {
+            return setError({ label: "N", message: "Слишком большое значение для N! Сделайте, пожалуйста, его меньше или равным 100" })
+        }
 
-  const [C2, setC2] = useState(DEFAULT.C2)
-  const onC2Change = (e: ChangeEvent<HTMLInputElement>) => {
-    setC2(Number(e.target.value))
-  }
+        if (N < 0) {
+            return setError({ label: "N", message: "Сделайте, пожалуйста, N неотрицательным" })
+        }
 
-  const [C3, setC3] = useState(DEFAULT.C3)
-  const onC3Change = (e: ChangeEvent<HTMLInputElement>) => {
-    setC3(Number(e.target.value))
-  }
-
-  const [S1, setS1] = useState(DEFAULT.S1)
-  const onS1Change = (e: ChangeEvent<HTMLInputElement>) => {
-    setS1(Number(e.target.value))
-  }
-
-  const [S, setS] = useState(DEFAULT.S)
-  const onSChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setS(Number(e.target.value))
-  }
-
-  const [Nzn, setNzn] = useState(DEFAULT.Nzn)
-  const onNznChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const Nzn= Number(e.target.value);
-
-    if (Nzn > 10) {
-        return setError({ label: "Nzn", message: "Слишком большое значение для Nzn! Сделайте, пожалуйста, его меньше 10" })
+        clearErrorsForInput('N')
+        setN(N)
     }
 
-    clearErrorsForInput('Nzn')
-    setNzn(Nzn)
-  }
+    const [C1, setC1] = useState(DEFAULT.C1)
+    const onC1Change = (e: ChangeEvent<HTMLInputElement>) => {
+        setC1(Number(e.target.value))
+    }
+
+    const [C2, setC2] = useState(DEFAULT.C2)
+    const onC2Change = (e: ChangeEvent<HTMLInputElement>) => {
+        setC2(Number(e.target.value))
+    }
+
+    const [C3, setC3] = useState(DEFAULT.C3)
+    const onC3Change = (e: ChangeEvent<HTMLInputElement>) => {
+        setC3(Number(e.target.value))
+    }
+
+    const [S1, setS1] = useState(DEFAULT.S1)
+    const onS1Change = (e: ChangeEvent<HTMLInputElement>) => {
+        setS1(Number(e.target.value))
+    }
+
+    const [S, setS] = useState(DEFAULT.S)
+    const onSChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setS(Number(e.target.value))
+    }
+
+    const [Nzn, setNzn] = useState(DEFAULT.Nzn)
+    const onNznChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const Nzn = Number(e.target.value);
+
+        if (Nzn > 10) {
+            return setError({ label: "Nzn", message: "Слишком большое значение для Nzn! Сделайте, пожалуйста, его меньше 10" })
+        }
+
+        clearErrorsForInput('Nzn')
+        setNzn(Nzn)
+    }
 
     const w = to / tno;
 
@@ -253,50 +255,78 @@ function App() {
     const TcValues = TpValues.map((Tpi) => mathTc(Tpi, tno));
     const peValues = TcValues.map((Tci) => mathPe(Tci, tno));
     const WValues = TpValues.map((Tpi) => mathW(Tpi, to));
+    const YValues = CValues.map((Ci, i) => Ci * S1 + LValues[i] * S)
 
     const hasErrors = (label: string) => !!errors.find((error) => error.label === label);
 
-  return (
-    <>
-      <form className="read-the-docs">
-        {[
-            { label: 'tno', value: tno, onChange: onTnoChange },
-            { label: "to", value: to, onChange: onToChange },
-            { label: "N", value: N, onChange: onNChange },
-            { label: "C1", value: C1, onChange: onC1Change },
-            { label: "C2", value: C2, onChange: onC2Change },
-            { label: "C3", value: C3, onChange: onC3Change },
-            { label: "S1", value: S1, onChange: onS1Change },
-            { label: "S", value: S, onChange: onSChange },
-            { label: "Nzn", value: Nzn, onChange: onNznChange },
-        ].map((inputProps) => <Input isError={hasErrors(inputProps.label)} key={inputProps.label} {...inputProps} />)}
-      </form>
-      <table>
-       <thead>
-          <Tr header={true} label="C" values={CValues} />
-        </thead>
+    const data = useMemo(() => [{
+        id: 'C-Y',
+        data: [
+            {
+                x: C1,
+                y: YValues[0]
+            },
+            {
+                x: C2,
+                y: YValues[1],
+            },
+            {
+                x: C3,
+                y: YValues[2]
+            }
+        ]
+    }], [C1, C2, C3, YValues])
 
-        <tbody>
-            <Tr formatValue={formatValue} label="Po" values={CValues.map((Ci) => mathP0(Ci, N, w))} />
-            <Tr formatValue={formatValue} label="Q" values={QValues} />
-            <Tr formatValue={formatValue} label="L" values={LValues} />
+    return (
+        <>
+            <form className="read-the-docs">
+                {[
+                    { label: 'tno', value: tno, onChange: onTnoChange },
+                    { label: "to", value: to, onChange: onToChange },
+                    { label: "N", value: N, onChange: onNChange },
+                    { label: "C1", value: C1, onChange: onC1Change },
+                    { label: "C2", value: C2, onChange: onC2Change },
+                    { label: "C3", value: C3, onChange: onC3Change },
+                    { label: "S1", value: S1, onChange: onS1Change },
+                    { label: "S", value: S, onChange: onSChange },
+                    { label: "Nzn", value: Nzn, onChange: onNznChange },
+                ].map((inputProps) => <Input isError={hasErrors(inputProps.label)} key={inputProps.label} {...inputProps} />)}
+            </form>
+            <table>
+                <thead>
+                    <Tr header={true} label="C" values={CValues} />
+                </thead>
 
-            <Tr formatValue={formatValue} label="U" values={UValues} />
-            <Tr formatValue={formatValue} label="po" values={poValues} />
-            <Tr formatValue={formatValue} label="n" values={nValues} />
+                <tbody>
+                    <Tr formatValue={formatValue} label="Po" values={CValues.map((Ci) => mathP0(Ci, N, w))} />
+                    <Tr formatValue={formatValue} label="Q" values={QValues} />
+                    <Tr formatValue={formatValue} label="L" values={LValues} />
 
-            <Tr formatValue={formatValue} label="pe" values={peValues} />
-            <Tr formatValue={formatValue} label="W" values={WValues} />
-            <Tr formatValue={formatValue} label="Tp" values={TpValues} />
-            <Tr formatValue={formatValue} label="Tc" values={TcValues} />
+                    <Tr formatValue={formatValue} label="U" values={UValues} />
+                    <Tr formatValue={formatValue} label="po" values={poValues} />
+                    <Tr formatValue={formatValue} label="n" values={nValues} />
 
-            <Tr formatValue={formatValue} label="pe/po" values={peValues.map((pei, i) => pei / poValues[i])} />
+                    <Tr formatValue={formatValue} label="pe" values={peValues} />
+                    <Tr formatValue={formatValue} label="W" values={WValues} />
+                    <Tr formatValue={formatValue} label="Tp" values={TpValues} />
+                    <Tr formatValue={formatValue} label="Tc" values={TcValues} />
 
-            <Tr formatValue={formatValue} label="Y" values={CValues.map((Ci, i) => Ci * S1 + LValues[i] * S)} />
-        </tbody>
-      </table>
-    </>
-  )
+                    <Tr formatValue={formatValue} label="pe/po" values={peValues.map((pei, i) => pei / poValues[i])} />
+
+                    <Tr formatValue={formatValue} label="Y" values={YValues} />
+                </tbody>
+            </table>
+            <Chart data={data} />
+        </>
+    )
+}
+
+const App = () => {
+    switch (window.location.pathname) {
+        case '/': return <TitlePage />
+        case '/main': return <MainPage />
+        default: return <span>key</span>
+    }
 }
 
 export default App
