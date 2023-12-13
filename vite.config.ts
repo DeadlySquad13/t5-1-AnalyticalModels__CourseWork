cf597f36a4b7827257a9d5ceed69879c3d457712
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-12-13 15:28:16 +0300

    [Build]: Added proper .env parsing to vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
-
-const BASE_URL = import.meta.env.VITE_BASE_URL || '';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: BASE_URL,
-  plugins: [react({
-    // babel: {
-        // presets: [],
-        // plugins: [],
-        // Use .babelrc files.
-        // babelrc: true,
-        // Use babel.config.js files.
-        // configFile: true,
-    // }  
-    }),
-    legacy({
-        targets: ['IE 8'],
-    })
-  ],
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+        base: env.VITE_BASE_URL || "./",
+        plugins: [
+            react({
+                // babel: {
+                // presets: [],
+                // plugins: [],
+                // Use .babelrc files.
+                // babelrc: true,
+                // Use babel.config.js files.
+                // configFile: true,
+                // }
+            }),
+            legacy({
+                targets: ["IE 8"],
+            }),
+        ],
+    };
+});
