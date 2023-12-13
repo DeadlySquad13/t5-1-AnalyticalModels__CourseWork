131bb76d243ee25e2632cdd63ecde74f1546f78a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-12-13 15:05:21 +0300

    [Build]: Added base url configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 
+const BASE_URL = import.meta.env.VITE_BASE_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_URL,
   plugins: [react({
     // babel: {
         // presets: [],
